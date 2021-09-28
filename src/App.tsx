4ded07856b15917376d48d2f41c69a68f729388b
Volodymyr Ionnikov <workingionn@gmail.com>
2021-09-28 12:09:11 +0300

    test 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  const a = 'asdasd';
+function App(): JSX.Element {
   return (
     <div className='App'>
       <header className='App-header'>
