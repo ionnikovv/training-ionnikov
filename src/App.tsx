0939b485be30002d5511e2e2862cb119cde73655
Volodymyr Ionnikov <workingionn@gmail.com>
2021-09-28 12:02:45 +0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,6 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const as = 'asdasdasdasasdasdas';
-
-  const b = '1231232asdaasasdasddasdasdasdd1asfgsdgfsdf';
-
   return (
     <div className='App'>
       <header className='App-header'>
