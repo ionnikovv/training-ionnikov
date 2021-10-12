273f9d75ed6a4290e60ab1b80b17800794de8ff2
Volodymyr Ionnikov <workingionn@gmail.com>
2021-10-12 11:49:43 +0300

    Changed state

--- src/components/StateForGame.tsx
+++ src/components/StateForGame.tsx
@@ -1,23 +1,23 @@
 import { useState } from 'react';
 
 export const StateForGame = (): JSX.Element => {
-  const [playerCoords, setPlayerCoords] = useState({ x: 0, y: 0 });
-  //   x is const for player
+  const [playerJumpValue] = useState(100);
 
-  const [obstacleHeight, setObstacleHeight] = useState({ height: 0 });
-  //   max-height for obstacle is height of player including jumping
+  const [obstacleHeight] = useState({ height: 0 });
 
-  const [obstacleCoords, setObstacleCoords] = useState({ x: 0, y: 0 });
-  //   after creating of component its 'y' will be a const
+  const [obstacleCoords] = useState({ x: 0 });
 
-  const [backgroundSpeed, setBackgroundSpeed] = useState('50px');
+  const backgroundSpeed = 50;
 
-  const [isPlaying, setIsPlaying] = useState(false);
-  // is game started
+  const [isPlaying] = useState(false);
 
-  const [score, setScore] = useState('50px');
-  // result of player`s game session
-  const [record, setRecord] = useState(0);
-
-  return <div>State</div>;
+  return (
+    <div>
+      <span>Player jump value: {playerJumpValue}</span>
+      <span>height of obstacle: {obstacleHeight.height}</span>
+      <span>Coords of obstacle transition: {obstacleCoords.x}</span>
+      <span>Speed of transition of background: {backgroundSpeed}</span>
+      <span>Is game started: {isPlaying}</span>
+    </div>
+  );
 };
