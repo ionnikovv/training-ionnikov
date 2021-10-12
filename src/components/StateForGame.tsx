764438d3ebf4297c7e6e370631720f189d3701a1
Volodymyr Ionnikov <workingionn@gmail.com>
2021-10-12 12:16:13 +0300

    Added count of obstacles state

--- src/components/StateForGame.tsx
+++ src/components/StateForGame.tsx
@@ -1,21 +1,19 @@
 import { useState } from 'react';
+import { backgroundSpeed } from '../ConstantValues/ConstValues';
 
 export const StateForGame = (): JSX.Element => {
-  const [playerJumpValue] = useState(100);
-
+  const [playerCoord] = useState(100);
+  const [obstaclesOnScreen] = useState([true, true]);
   const [obstacleHeight] = useState(0);
-
   const [obstacleShifting] = useState(50);
-
-  const backgroundSpeed = 50;
-
   const [isPlaying] = useState(false);
 
   return (
     <div>
-      <span>Player jump value: {playerJumpValue}</span>
-      <span>height of obstacle: {obstacleHeight}</span>
-      <span>Coords of obstacle transition: {obstacleShifting}</span>
+      <span>Player jump value: {playerCoord}</span>
+      <span>Height of obstacle: {obstacleHeight}</span>
+      <span>Count of obstacles on screen :{obstaclesOnScreen}</span>
+      <span>Coords of obstacle shifting: {obstacleShifting}</span>
       <span>Speed of transition of background: {backgroundSpeed}</span>
       <span>Is game started: {isPlaying}</span>
     </div>
