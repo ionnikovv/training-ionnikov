cd896f6728785f20e1bedfa11eb026fbea372921
Volodymyr Ionnikov <workingionn@gmail.com>
2021-10-12 12:32:03 +0300

    Count of obstacles on screen

--- src/components/StateForGame.tsx
+++ src/components/StateForGame.tsx
@@ -1,17 +1,18 @@
 import { useState } from 'react';
 import { backgroundSpeed } from '../ConstantValues/ConstValues';
+import { Obstacle } from './Game/Obstacle/Obstacle';
 
 export const StateForGame = (): JSX.Element => {
   const [playerCoord] = useState(100);
-  const [obstaclesOnScreen] = useState([]);
-  const [obstacleHeight] = useState(0);
+  const [obstaclesOnScreen] = useState([10, 20, 30]);
+  //   this array consist of each obstacle`s height
   const [obstacleShifting] = useState(50);
   const [isPlaying] = useState(false);
 
   return (
     <div>
       <span>Player jump value: {playerCoord}</span>
-      <span>Height of obstacle: {obstacleHeight}</span>
+      <span>count of obstacles and their height : {obstaclesOnScreen}</span>
       <span>Count of obstacles on screen :{obstaclesOnScreen}</span>
       <span>Coords of obstacle shifting: {obstacleShifting}</span>
       <span>Speed of transition of background: {backgroundSpeed}</span>
