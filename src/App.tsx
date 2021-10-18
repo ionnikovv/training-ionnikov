8b0c97e9d2f7131c5e08946da6e35d3e4fe70a9d
Volodymyr Ionnikov <workingionn@gmail.com>
2021-10-18 14:18:22 +0300

    Refactoring of App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,52 +4,33 @@ import { Game } from './components/Game/Game';
 import { SelectPokemons } from './components/SelectPokemons/SelectPokemons';
 import { PokemonsData } from './types/PokemonsData';
 
-type Props = {
-  disabledPokemons?: string[];
-  selectedPokemonSize?: number;
-};
-
-export const App = ({ disabledPokemons = [''], selectedPokemonSize = 1 }: Props): JSX.Element => {
-  const [isSelected, setIsSelected] = useState(false);
+export const App = (): JSX.Element => {
   const [isGameStarted, setIsGameStarted] = useState(false);
   const [pokemonPlayer, setPokemonPlayer] = useState<PokemonsData>();
-  const [selectedPokemons, setSelectedPokemons] = useState<string[]>([]);
-
-  const onSelectPokemon = (pokemon: PokemonsData, isCurrentlySelected: boolean): void => {
-    setPokemonPlayer(pokemon);
-    if (isCurrentlySelected) {
-      setIsSelected(!isSelected);
-      setSelectedPokemons(selectedPokemons.filter((item) => item !== pokemon.name));
-    } else {
-      if (disabledPokemons.includes(pokemon.name)) return;
-
-      if (selectedPokemons.length >= selectedPokemonSize)
-        return setSelectedPokemons([...selectedPokemons.slice(1), pokemon.name]);
-
-      setIsSelected(!isSelected);
-      setSelectedPokemons([...selectedPokemons, pokemon.name]);
-    }
-  };
 
   return (
     <div className='main-wrapper'>
-      {isSelected && (
-        <div className='btn-start-wrapper'>
-          <button
-            className='btn-start'
-            onClick={() => {
-              setIsSelected(false);
-              setIsGameStarted(true);
-            }}
-          >
-            Start
-          </button>
-        </div>
-      )}
       {isGameStarted ? (
         <Game pokemonPlayer={pokemonPlayer} />
       ) : (
-        <SelectPokemons selectedPokemons={selectedPokemons} onSelectPokemon={onSelectPokemon} />
+        <div>
+          <SelectPokemons
+            selectedPokemons={pokemonPlayer ? [pokemonPlayer.name] : []}
+            onSelectPokemon={setPokemonPlayer}
+          />
+          {pokemonPlayer && (
+            <div className='btn-start-wrapper'>
+              <button
+                className='btn-start'
+                onClick={() => {
+                  setIsGameStarted(true);
+                }}
+              >
+                Start
+              </button>
+            </div>
+          )}
+        </div>
       )}
     </div>
   );
