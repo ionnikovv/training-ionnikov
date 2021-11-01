18e763a9e0831280f753c533edaadb063928f0ec
Volodymyr Ionnikov <workingionn@gmail.com>
2021-11-01 17:46:28 +0200

    Fixed webcamera

--- src/components/CameraComponent/Camera.tsx
+++ src/components/CameraComponent/Camera.tsx
@@ -1,20 +1,10 @@
 import React, { useEffect, useRef, useState } from 'react';
-// import * as posenet from '@tensorflow-models/posenet';
+import * as posenet from '@tensorflow-models/posenet';
 import './Camera.css';
 
 export const Camera = (): JSX.Element => {
   const videoRef = useRef<HTMLVideoElement>(null);
   const [videoStream, setVideoStream] = useState<MediaStream | null>(null);
-
-  // navigator.mediaDevices
-  //   .getUserMedia({ video: true, audio: false })
-  //   .then(function (stream) {
-  //     video.srcObject = stream;
-  //     video.play();
-  //   })
-  //   .catch(function (err) {
-  //     console.log('An error occurred: ' + err);
-  //   });
 
   useEffect(() => {
     const webcamera = videoRef.current;
@@ -22,17 +12,17 @@ export const Camera = (): JSX.Element => {
     webcamera.srcObject = videoStream;
     webcamera.play();
   }, [videoRef, videoStream]);
+
   useEffect(() => {
     const playVideo = async () => {
       setVideoStream(await navigator.mediaDevices.getUserMedia({ video: true }));
-      if (!videoRef) return;
     };
-  }, [videoRef, videoStream]);
+    playVideo();
+  }, []);
+
   return (
     <div className='webcamera-wrapper'>
-      <video ref={videoRef} width={1200} height={1000} className={'webcamera'}>
-        it is doen`t works
-      </video>
+      <video ref={videoRef} width={1200} height={1000} className={'webcamera'} />
     </div>
   );
 };
